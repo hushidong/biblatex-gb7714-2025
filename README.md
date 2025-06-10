7199d4239420ff88a188629c4ac321c07887b500
zhen hu <hzzmail@sjtu.org>
2025-06-10 23:13:15 +0800

    update-to-v1.1v

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<b>Date of last change: 2025/05/22 v1.1u</b>
+<b>Date of last change: 2025/06/10 v1.1v</b>
 
 
 
@@ -90,7 +90,7 @@ gbT7714-2025.pdf 由gbT7714-2025.tex编译得到。
 * 2025/03/03 v1.1t,github,initialized
 
 * 2025/05/22 v1.1u,ctan,github
-
+* 2025/06/10 v1.1v,ctan,github
 
 
 
--- makefileGBT2025.bat
+++ makefileGBT2025.bat
@@ -5,13 +5,6 @@
 title %date% %time% makefileGBT2025
 
 
-xelatex.exe  testeg.tex
-
-biber testeg
-
-xelatex.exe  testeg.tex
-
-
 xelatex.exe  tngbcitationaynew.tex
 
 biber tngbcitationaynew
@@ -26,4 +19,18 @@ biber gbT7714-2025
 xelatex.exe  gbT7714-2025.tex
 
 xelatex.exe  gbT7714-2025.tex
+
+
+xelatex.exe  testeg.tex
+
+biber testeg
+
+xelatex.exe  testeg.tex
+
+
+xelatex.exe  eg-molc.tex
+
+biber eg-molc
+
+xelatex.exe  eg-molc.tex
 
